9de5cccfbb98b856d9102c1c75b87c8a8e685fd2
khalil <salamikhalil02@gmail.com>
2023-04-25 19:59:07 +0100

    font update

---
+++ src/components/cart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cart() {
+	return (
+		<section className="bg-black/60 absolute inset-0">
+			<div className="bg-white "></div>
+		</section>
+	);
+}
+
+export default Cart;
