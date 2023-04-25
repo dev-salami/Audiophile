32fd99cbadc86432f7aa2a7fb4ad4c82811e7afc
khalil <salamikhalil02@gmail.com>
2023-04-25 19:58:35 +0100

    dummy data

---
+++ data.js
@@ -0,0 +1,58 @@
+const products = [
+	{
+		id: 1,
+		name: "Bose QuietComfort 35 II Wireless Bluetooth Headphones",
+		category: "Headphones",
+		details:
+			"Industry-leading noise cancellation, wireless connectivity, and 20 hours of battery life.",
+		features: [
+			"Noise-cancellation",
+			"Wireless connectivity",
+			"20 hours of battery life",
+		],
+		price: 299.99,
+		available: 20,
+	},
+	{
+		id: 2,
+		name: "Sonos One (Gen 2) Voice Controlled Smart Speaker",
+		category: "Speakers",
+		details:
+			"Alexa and Google Assistant voice control, premium sound quality, and multi-room listening.",
+		features: [
+			"Voice control",
+			"Premium sound quality",
+			"Multi-room listening",
+		],
+		price: 199.99,
+		available: 10,
+	},
+	{
+		id: 3,
+		name: "Sennheiser HD 660 S High-Resolution Audiophile Headphones",
+		category: "Headphones",
+		details:
+			"Open-back design, high-resolution sound quality, and comfortable fit for extended listening sessions.",
+		features: [
+			"Open-back design",
+			"High-resolution sound quality",
+			"Comfortable fit",
+		],
+		price: 499.99,
+		available: 5,
+	},
+	{
+		id: 4,
+		name: "Klipsch RP-500M Reference Premiere Bookshelf Speakers",
+		category: "Speakers",
+		details:
+			"High-efficiency drivers, detailed and dynamic sound, and a stylish design.",
+		features: [
+			"High-efficiency drivers",
+			"Detailed and dynamic sound",
+			"Stylish design",
+		],
+		price: 399.99,
+		available: 7,
+	},
+];
