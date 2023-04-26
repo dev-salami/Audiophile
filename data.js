5a84e15fcd7aa42b15e55636fd1f0f775ad8a0b0
khalil <salamikhalil02@gmail.com>
2023-04-26 23:59:55 +0100

    data update

--- data.js
+++ data.js
@@ -1,58 +1,135 @@
 const products = [
 	{
 		id: 1,
-		name: "Bose QuietComfort 35 II Wireless Bluetooth Headphones",
-		category: "Headphones",
+		name: "ZX9 SPEAKER",
+		category: "Speaker",
 		details:
-			"Industry-leading noise cancellation, wireless connectivity, and 20 hours of battery life.",
-		features: [
-			"Noise-cancellation",
-			"Wireless connectivity",
-			"20 hours of battery life",
+			"Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+		features:
+			"Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m). Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.",
+		box: [
+			{ type: "Speaker Unit", unit: "2X" },
+			{ type: "Speaker cloth panel", unit: "2X" },
+			{ type: "User manual", unit: "1X" },
+			{ type: "3.5mm 10m audio cable ", unit: "1X" },
+			{ type: "10m optical cable", unit: "1X" },
+		],
+		images: [
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447040/audiophile-asset/zx7-ii_fy5in8.jpg",
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447039/audiophile-asset/zx7-i_pnch8u.jpg",
 		],
 		price: 299.99,
 		available: 20,
 	},
 	{
 		id: 2,
-		name: "Sonos One (Gen 2) Voice Controlled Smart Speaker",
-		category: "Speakers",
+		name: "ZX9 SPEAKER",
+		category: "Speaker",
 		details:
-			"Alexa and Google Assistant voice control, premium sound quality, and multi-room listening.",
-		features: [
-			"Voice control",
-			"Premium sound quality",
-			"Multi-room listening",
+			"Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+		features:
+			"Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m). Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.",
+		box: [
+			{ type: "Speaker Unit", unit: "2X" },
+			{ type: "Speaker cloth panel", unit: "2X" },
+			{ type: "User manual", unit: "1X" },
+			{ type: "3.5mm 10m audio cable ", unit: "1X" },
+			{ type: "10m optical cable", unit: "1X" },
 		],
-		price: 199.99,
-		available: 10,
+		images: [
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447040/audiophile-asset/zx7-ii_fy5in8.jpg",
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447039/audiophile-asset/zx7-i_pnch8u.jpg",
+		],
+		price: 299.99,
+		available: 20,
 	},
 	{
 		id: 3,
-		name: "Sennheiser HD 660 S High-Resolution Audiophile Headphones",
-		category: "Headphones",
+		name: "ZX9 SPEAKER",
+		category: "Speaker",
 		details:
-			"Open-back design, high-resolution sound quality, and comfortable fit for extended listening sessions.",
-		features: [
-			"Open-back design",
-			"High-resolution sound quality",
-			"Comfortable fit",
+			"Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+		features:
+			"Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m). Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.",
+		box: [
+			{ type: "Speaker Unit", unit: "2X" },
+			{ type: "Speaker cloth panel", unit: "2X" },
+			{ type: "User manual", unit: "1X" },
+			{ type: "3.5mm 10m audio cable ", unit: "1X" },
+			{ type: "10m optical cable", unit: "1X" },
 		],
-		price: 499.99,
-		available: 5,
+		images: [
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447040/audiophile-asset/zx7-ii_fy5in8.jpg",
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447039/audiophile-asset/zx7-i_pnch8u.jpg",
+		],
+		price: 299.99,
+		available: 20,
 	},
 	{
 		id: 4,
-		name: "Klipsch RP-500M Reference Premiere Bookshelf Speakers",
-		category: "Speakers",
+		name: "ZX9 SPEAKER",
+		category: "Speaker",
 		details:
-			"High-efficiency drivers, detailed and dynamic sound, and a stylish design.",
-		features: [
-			"High-efficiency drivers",
-			"Detailed and dynamic sound",
-			"Stylish design",
+			"Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+		features:
+			"Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m). Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.",
+		box: [
+			{ type: "Speaker Unit", unit: "2X" },
+			{ type: "Speaker cloth panel", unit: "2X" },
+			{ type: "User manual", unit: "1X" },
+			{ type: "3.5mm 10m audio cable ", unit: "1X" },
+			{ type: "10m optical cable", unit: "1X" },
 		],
-		price: 399.99,
-		available: 7,
+		images: [
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447040/audiophile-asset/zx7-ii_fy5in8.jpg",
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447039/audiophile-asset/zx7-i_pnch8u.jpg",
+		],
+		price: 299.99,
+		available: 20,
+	},
+	{
+		id: 5,
+		name: "ZX9 SPEAKER",
+		category: "Speaker",
+		details:
+			"Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+		features:
+			"Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m). Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.",
+		box: [
+			{ type: "Speaker Unit", unit: "2X" },
+			{ type: "Speaker cloth panel", unit: "2X" },
+			{ type: "User manual", unit: "1X" },
+			{ type: "3.5mm 10m audio cable ", unit: "1X" },
+			{ type: "10m optical cable", unit: "1X" },
+		],
+		images: [
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447040/audiophile-asset/zx7-ii_fy5in8.jpg",
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447039/audiophile-asset/zx7-i_pnch8u.jpg",
+		],
+		price: 299.99,
+		available: 20,
+	},
+	{
+		id: 6,
+		name: "ZX9 SPEAKER",
+		category: "Speaker",
+		details:
+			"Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+		features:
+			"Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m). Discover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.",
+		box: [
+			{ type: "Speaker Unit", unit: "2X" },
+			{ type: "Speaker cloth panel", unit: "2X" },
+			{ type: "User manual", unit: "1X" },
+			{ type: "3.5mm 10m audio cable ", unit: "1X" },
+			{ type: "10m optical cable", unit: "1X" },
+		],
+		images: [
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447040/audiophile-asset/zx7-ii_fy5in8.jpg",
+			"https://res.cloudinary.com/drphqvmfe/image/upload/v1682447039/audiophile-asset/zx7-i_pnch8u.jpg",
+		],
+		price: 299.99,
+		available: 20,
 	},
 ];
+export default products;
